14376d4dff5d242e1cae13b04da43adab854368a
UTKARSH <utkarsht724>
2020-02-20 14:02:56 +0530

    Added:slice in tuple

---
+++ Tuple/Slice_tuple.py
@@ -0,0 +1,11 @@
+#program to slice a tuple
+tuple=(1,2,3,3,4,'xyz','abc')
+print("orginal tuple",tuple)
+tup=tuple[4:]
+print("after slicing",tup)
+tup=tuple[:4]
+print("after slicing",tup)
+tup=tuple[3:]
+print("after slicing",tup)
+tup=tuple[1:]
+print("after slicing",tup)
