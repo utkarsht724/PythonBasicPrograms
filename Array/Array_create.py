705a4b1b6d623eefc3ffc4fd32ae800417a87f91
UTKARSH <utkarsht724>
2020-02-20 12:03:24 +0530

    Added:Array Create

---
+++ Array/Array_create.py
@@ -0,0 +1,13 @@
+from array import *
+arr = array('i',[])
+for number in range(5):
+    x=int(input("enter the next element of array"))
+    arr.append(x)
+    print(arr)
+
+print("print first element",arr[0])
+print("print second element", arr[1])
+print("print third element", arr[2])
+print("print fourth element", arr[3])
+print("print fifth element",arr[4])
+
---
+++ Array/Reverse_Array.py
@@ -0,0 +1,9 @@
+from array import *
+arr = array('i',[])
+arr_length=int(input("enter the length of the array:"))
+for number in range(arr_length):
+    x=int(input("enter the next element of array"))
+    arr.append(x)
+for i in range(arr_length,arr[0]):
+ print(arr)
+
