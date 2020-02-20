4435660bfd23de68f98e941edacc4dff3b215062
UTKARSH <utkarsht724>
2020-02-20 12:34:12 +0530

    Added:Larger word in list

---
+++ List/larger_word.py
@@ -0,0 +1,8 @@
+def Larger_word():
+    list=[]
+    length=(input("enter the length"))
+    for i in range(length):
+        number=input("enter the element in the list")
+        list.append(number)
+    print(list)
+Larger_word()
