8778b7091dba7fcca9f3137d616da043717d16c5
UTKARSH <utkarsht724>
2020-02-20 14:29:42 +0530

    Added:count occurance of string

---
+++ String/count_occurance_substring.py
@@ -0,0 +1,5 @@
+#program to count occurance of substring in a string
+str= input("enter the element in string")
+print(str)
+substring=input("enter the substring which u want to count")
+print(str.count(substring))
