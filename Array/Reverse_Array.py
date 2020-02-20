362024dce3383565c15426fa2f6332b607da161c
UTKARSH <utkarsht724>
2020-02-20 12:07:20 +0530

    Added:Reverse Array

--- Array/Reverse_Array.py
+++ Array/Reverse_Array.py
@@ -1,9 +1,8 @@
 from array import *
 arr = array('i',[])
-arr_length=int(input("enter the length of the array:"))
+arr_length=int(input("enter the Length of the array:"))
 for number in range(arr_length):
     x=int(input("enter the next element of array"))
     arr.append(x)
 for i in range(arr_length,arr[0]):
  print(arr)
-
