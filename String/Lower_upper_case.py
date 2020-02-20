4c304973925e15dcc4768d312890e58b5769a7b7
UTKARSH <utkarsht724>
2020-02-20 14:33:23 +0530

    Added:print string in lower&uppercase

---
+++ String/Lower_upper_case.py
@@ -0,0 +1,6 @@
+def lower_upper():
+    str=(input("enter the element "))
+    print("orginal string:",str)
+    print("Upper case:",str.upper())
+    print("Lower case:",str.lower())
+lower_upper()
