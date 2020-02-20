b1cb6750d56e6258ff6485ae0e8809b4390429fb
UTKARSH <utkarsht724>
2020-02-20 14:55:38 +0530

    Added:print get in bytes

---
+++ Basic_Programs/get_in_bytes.py
@@ -0,0 +1,11 @@
+import sys
+string1="one"
+string2="four"
+print()
+print("Memory size of '"+string1+"' = "+str(sys.getsizeof(string1))+ "bytes")
+print("Memory size of '"+string2+"' = "+str(sys.getsizeof(string2))+ "bytes")
+print()
+
+
+
+
