a07030985d643567771d5f4e6669fe72739d362f
UTKARSH <utkarsht724>
2020-02-20 11:50:49 +0530

    Added:count no value repeated in multiple dictionary

---
+++ Dictionary/iterate_over_dictonary.py
@@ -0,0 +1,7 @@
+#program to itearte over dictionary
+def iteration_dic():
+    a_dict={"name":"xyz","age":23,"sex":"male"}
+    for key in a_dict:
+        print(key,"-",a_dict[key])
+
+iteration_dic()
