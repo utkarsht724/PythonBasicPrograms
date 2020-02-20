ab24804106b8f7c87a42938cad97b9bade17003b
UTKARSH <utkarsht724>
2020-02-20 14:11:37 +0530

    Added:Different item in tuple

---
+++ Tuple/tuple_different.py
@@ -0,0 +1,19 @@
+#create tuple with different data types
+tuple=(1,2,'abc','xyz',2.5,5,99,[2,3],[9])
+print(tuple)
+print(" ")
+
+tup_integer=(1,2)
+print("integer",tup_integer)
+print(" ")
+
+tup_float=(2.5,5.99)
+print("float",tup_float)
+print(" ")
+
+tup_string=('xyz','abc')
+print("string",tup_string)
+print(" ")
+
+tup_list=([2,3],[5,4])
+print("string",tup_list)
