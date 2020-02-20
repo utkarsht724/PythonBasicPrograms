183721aba8396260415255304e34a189b9ac6847
UTKARSH <utkarsht724>
2020-02-20 11:00:04 +0530

    Added:Add to a key program

---
+++ Dictionary/Addkey_dictionary.py
@@ -0,0 +1,9 @@
+#program to add elemnet in a dictionary
+def add_element():
+    Sample_Dictionary={0:10,1:20}
+    print("Sample Dictionary",Sample_Dictionary)
+                                             #ADD THE ELEMENT
+    Sample_Dictionary[2]=30
+
+    print("Expected Dictionary",Sample_Dictionary)
+add_element()
