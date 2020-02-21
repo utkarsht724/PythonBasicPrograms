f6d440b54d541881ab31974a302c35e7823cb2a8
UTKARSH <utkarsht724>
2020-02-21 11:46:30 +0530

    sort the list

--- Basic_Programs/sort.py
+++ Basic_Programs/sort.py
@@ -1,2 +1,7 @@
-list=[1,9,3]
-print(sorted(list))
+#program to sort the list with taking  3  elements
+class list:
+    def lst(self):
+       list=[1,9,3,]
+       print(sorted(list))
+l = list()
+l.lst()
