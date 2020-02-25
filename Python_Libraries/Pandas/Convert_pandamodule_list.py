61c6f0a1d9535ecdef560a412850662187b3888e
UTKARSH <utkarsht724>
2020-02-25 18:42:16 +0530

    Added:Convert panda module into list

---
+++ Python_Libraries/Pandas/Convert_pandamodule_list.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+series=pd.Series([2,4,6,8,10])
+print("SERIES",series)
+print()
+print("TYPE SERIES",type(series))
+print()
+print("CONVERT TO LIST",series.tolist())
+print()
+print("LIST TYPE",type(series.tolist()))
+print()
