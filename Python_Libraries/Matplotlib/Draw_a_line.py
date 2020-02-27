c0eca55c4890254457dae2997b0cf610641c95a9
UTKARSH <utkarsht724>
2020-02-27 19:14:21 +0530

    Added:Draw a line

---
+++ Python_Libraries/Matplotlib/Draw_a_line.py
@@ -0,0 +1,7 @@
+#Write a Python program to draw a line with suitable label in the x axis, y axis and a title
+from matplotlib import pyplot as plt
+plt.plot([5,8,10])
+plt.title('info')
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.show()
