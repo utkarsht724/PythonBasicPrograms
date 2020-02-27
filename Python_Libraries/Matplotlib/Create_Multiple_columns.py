96b9be568b5d5e67aba7eac710f71e9302c27edf
UTKARSH <utkarsht724>
2020-02-27 19:05:06 +0530

    Added:Create multiple columns

---
+++ Python_Libraries/Matplotlib/Create_Multiple_columns.py
@@ -0,0 +1,29 @@
+#Write a Python program to create multiple plots
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+fig.subplots_adjust(bottom=0.020, left=0.020, top = 0.900, right=0.800 )
+
+
+plt.subplot(2, 2, 1)   # subplot(nrows(length), ncols(length), index)
+
+plt.xticks(())
+plt.yticks(())
+
+plt.subplot(2, 3, 4)
+plt.xticks(())
+plt.yticks(())
+
+plt.subplot(2, 3, 5)
+plt.xticks(())
+plt.yticks(())
+
+plt.subplot(2, 3, 6)
+plt.xticks(())
+plt.yticks(())
+
+plt.show()
+
+
+
